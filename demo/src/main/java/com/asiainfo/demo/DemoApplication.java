968574481d11eff5588b811938eb974541d998bd
chenjq <chenjq@asiainfo.com>
2018-12-21 15:56:41 +0800

    config-server

---
+++ demo/src/main/java/com/asiainfo/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.asiainfo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
